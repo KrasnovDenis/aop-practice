7b29cdf7921891c956da4034271ca16ec0c990e5
Denis O. Krasnov <elo.1972@yandex.ru>
2021-10-14 02:11:02 +0400

    Explain pointcuts

--- src/main/java/com/krasnov/example/aop_practice/aspect/SpringAOPAspect.java
+++ src/main/java/com/krasnov/example/aop_practice/aspect/SpringAOPAspect.java
@@ -1,44 +1,98 @@
 package com.krasnov.example.aop_practice.aspect;
 
-//@Aspect
-//@Component
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class SpringAOPAspect {
-/*
+
     @Before("execution(* com.krasnov.example.aop_practice.old.audit.ExampleOldAudit.*(..))")
-    public void basedOnRegExp(){
-        System.out.println("REGEXP");
+    public void basedOnRegExp() {
     }
 
     @Before("@annotation(com.krasnov.example.aop_practice.Loggable)")
-    public void basedOnAnnotation(JoinPoint joinPoint){
-        System.out.println(joinPoint.getKind());
+    public void basedOnAnnotation(JoinPoint joinPoint) {
     }
 
-    @Before("pointCut()")
-    public void basedOnPointCut(){
-        System.out.println("before Annotation pointcut");
+    @Before("pointCutExecution()")
+    public void basedOnPointCut() {
     }
 
-    @After("pointCut()")
-    public void afterBasedOnPointCut(){
-        System.out.println("After");
+    @After("pointCutExecution()")
+    public void afterBasedOnPointCut() {
     }
 
-    @AfterReturning("pointCut()")
-    public void afterReturningBasedOnPointCut(){
-        System.out.println("After return");
+    @AfterReturning("pointCutExecution()")
+    public void afterReturningBasedOnPointCut() {
     }
 
-    @Around("pointCut()")
+    @Around("pointCutExecution()")
     public Object aroundBasedOnPointCut(ProceedingJoinPoint jp) throws Throwable {
-        System.out.println("Before proceed");
+        System.out.println("Before proceed time is - " + System.currentTimeMillis() + "ms.");
         jp.proceed();
-        System.out.println("After proceed");
+        System.out.println("After proceed" + System.currentTimeMillis() + "ms.");
         return jp;
     }
 
-    @Pointcut("@annotation(com.krasnov.example.aop_practice.Loggable)")
-    public void pointCut(){}
 
- */
+    /**
+     * <p> Ищет по имени метода (явно или по регулярке *)</p>
+     * param method name by full signature
+     */
+    @Pointcut("execution(* com.krasnov.example.aop_practice.old.audit.ExampleOldAudit.*(..))")
+    public void pointCutExecution() {
+    }
+
+    /**
+     * <p> Берет все методы из данного класса (f.e. ExampleOldAudit)</p>
+     * Можно также указывать с регуляркой, для пакетов
+     * param classname
+     */
+    @Pointcut("within(com.krasnov.example.aop_practice.old.audit.ExampleOldAudit)")
+    public void pointCutWithIn() {
+    }
+
+    /**
+     * <p>Берет экземпляры данного объекта - можно указать интерфейс</p>
+     */
+    @Pointcut("target(org.slf4j.Logger)")
+    public void pointCutTarget() {
+    }
+
+    /**
+     * <p>То же самое как и target, но применительно к аннотациям</p>
+     * param annotation
+     */
+    @Pointcut("@target(com.krasnov.example.aop_practice.Loggable)")
+    public void pointCutTarget2() {
+    }
+
+    /**
+     * <p>Берет методы, у которых совпал аргумент с экзменпляром нашего</p>
+     * param classname
+     */
+    @Pointcut("args(java.util.List)")
+    public void pointCutArgs() {
+    }
+
+    /**
+     * <p>Метод, где принимается аннотация RequestParam в виде аргумента </p>
+     * param annotation
+     */
+    @Pointcut("@args(org.springframework.web.bind.annotation.RequestParam)")
+    public void pointCutArgs2() {
+    }
+
+    /**
+     * <p> Ищет метод над которым стоит аннотация Loggable</p>
+     * param annotation
+     */
+    @Pointcut("@annotation(com.krasnov.example.aop_practice.Loggable)")
+    public void pointCutAnnotation2() {
+    }
+
+
 }
